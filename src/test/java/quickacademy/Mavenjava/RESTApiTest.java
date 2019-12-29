1c3d071f00fcb67e14ff131b383dd5e743b130a4
Udani <isuru.udani1@gmail.com>
2019-12-29 22:27:16 +0800

    initial

---
+++ src/test/java/quickacademy/Mavenjava/RESTApiTest.java
@@ -0,0 +1,19 @@
+package quickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
